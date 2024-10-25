4a30a82e861946102d1f33b7f15e605524a70791
Yusra <yusra13579khan@gmail.com>
2024-10-25 14:36:35 +0530

    steps is updated

--- js/index.js
+++ js/index.js
@@ -232,7 +232,7 @@ document.getElementById('contactForm').addEventListener('submit', async (event) 
     const message = document.getElementById('message').value;
 
     try {
-        const response = await fetch('http://localhost:5000/api/contact', {
+        const response = await fetch('https://backend-steps-1.onrender.com/api/contact', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
